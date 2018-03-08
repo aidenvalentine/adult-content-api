bc1fe459d8000d5e8a2e14462c1c73025c8d878b
Aiden Valentine <txm-nero@users.noreply.github.com>
2018-03-08 04:09:03 -0500

    Adds ability to query c4s sales reports using query string params.

--- manyvids/routes/clips4sale/sales/index.js
+++ manyvids/routes/clips4sale/sales/index.js
@@ -5,9 +5,32 @@ var jsonParser = bodyParser.json()
 // clips4sale Helper
 const salesHelper = require('./salesHelper.js');
 
+// test data
+// var params = {
+//   s_year: 2018,
+//   s_month: 03,
+//   s_day: 01,
+//   e_year: 2018,
+//   e_month: 03,
+//   e_day: 08,
+// };
+
 // Test Route
+/**
+ * [exports description]
+ * @type {[type]}
+ * @query {Integer} s_year Start Year
+ * @query {Integer} s_month Start Month
+ * @query {Integer} s_day Start Day
+ * @query {Integer} e_year Start Year
+ * @query {Integer} e_month Start Month
+ * @query {Integer} e_day Start Day
+ * @query {String} report_type=detail1 Report Type
+ * @query {String} stores=all Stores
+ * @query {String} action=report Action
+ */
 sales.get('/', (req, res) => {
-  salesHelper.getReport(params, (err, data) => {
+  salesHelper.getReport(req.query, (err, data) => {
     if (err) { callback(err, data); };
     console.log(data);
     res.status(200).json(data);
