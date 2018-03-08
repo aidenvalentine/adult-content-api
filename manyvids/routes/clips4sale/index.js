e14165792ab659bfca6fe4b0e745926a2c8f5e8b
Aiden Valentine <txm-nero@users.noreply.github.com>
2018-03-07 23:54:13 -0500

    Adds c4s postClip() method to RESTful Express API.

--- manyvids/routes/clips4sale/index.js
+++ manyvids/routes/clips4sale/index.js
@@ -1,4 +1,6 @@
 var clips4sale = require('express').Router({ mergeParams: true });
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 // clips4sale Helper
 const c4s = require('./c4sHelper.js');
@@ -19,6 +21,28 @@ clips4sale.get('/clips', function (req, res) {
   var id = req.params.id;
   console.log(`GET /clips - Mock Endpoint`); // Mock
   res.json({});
+});
+
+// route to trigger the capture
+clips4sale.post('/clips', jsonParser, function (req, res) {
+  var event = req.body;
+  console.log(`POST /clips - Mock Endpoint`); // Mock
+  console.log(JSON.stringify(event, null, 2)); // Mock
+  // res.json({}); // Mock Response
+  var credentials = {
+    user: process.env.C4S_USER,
+    pass: process.env.C4S_PASS
+  };
+  const params = {
+    client: client,
+    cookie: cookie
+  };
+  c4s.login(credentials, params, function(err, data) {
+    c4s.postClip(event, params, function(err, data) {
+      console.log(data);
+      res.json(data);
+    });
+  });
 });
 
 // route to trigger the capture
@@ -44,13 +68,6 @@ clips4sale.get('/clips/:id', function (req, res) {
 
   });
 
-});
-
-// route to trigger the capture
-clips4sale.post('/clips/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(`POST /clips/${id} - Mock Endpoint`); // Mock
-  res.json({});
 });
 
 // route to trigger the capture
